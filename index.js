5224b8cf73ca12b37b516d1bebd1ff207ab98226
sazzadul1205 <psazzadul@gmail.com>
2023-11-05 12:35:02 +0600

    bid get, bid id get and bid push compleat

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const port = process.env.PORT || 5000;
 require('dotenv').config();
 
 
+
 //parsers
 app.use(express.json())
 app.use(cookieParser())
@@ -17,7 +18,7 @@ app.use(cors({
 }))
 
 
-const uri = "mongodb+srv://psazzadul1205:1jOikDWMHRkiebal@job-shop.scmo9ih.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@job-shop.scmo9ih.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -35,16 +36,87 @@ async function run() {
 
         // collections 
         const jobsCollection = client.db('job-shop').collection('jobs')
+        const bidsCollection = client.db('job-shop').collection('bids')
 
+
+
+        // middleware to see if the token is valid
+        const gateman = (req, res, next) => {
+            const { token } = req.cookies
+            if (!token) {
+                return res.status(401).send({ message: 'You are not authorized' })
+            }
+            jwt.verify(token, secret, function (err, decoded) {
+                if (err) {
+                    return res.status(401).send({ message: 'You are not authorized' })
+                }
+                req.user = decoded
+                next()
+            });
+
+
+        }
+
+        // token genaration
+        app.post('/api/v1/auth/access-token', (req, res) => {
+            const user = req.body
+            console.log(user);
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.cookie('token', token, {
+                httpOnly: true,
+                secure: true,
+                sameSite: 'none'
+            }).send({ success: true })
+        })
+
+        // jobs section
 
         // show all the jobs
         app.get('/api/v1/jobs', async (req, res) => {
-            const cursor = jobsCollection.find()
+
+            // sorting by category
+            let queryObj = {}
+            const category = req.query.category
+            if (category) {
+                queryObj.category = category;
+            }
+
+            const cursor = jobsCollection.find(queryObj)
             const result = await cursor.toArray()
 
             res.send(result)
         })
 
+
+        // show the individual jobs by id
+        app.get('/api/v1/jobs/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await jobsCollection.findOne(query);
+            res.send(result);
+        });
+
+        // bid section
+        // show all the bids
+        app.get('/api/v1/bids', async (req, res) => {
+            const cursor = bidsCollection.find()
+            const result = await cursor.toArray()
+
+            res.send(result)
+        })
+        // show the individual bids by id
+        app.get('/api/v1/bids/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await bidsCollection.findOne(query);
+            res.send(result);
+        });
+        app.post('/api/v1/bids/add-new-bid', async (req, res) => {
+            const bid = req.body;
+            console.log(bid);
+            const result = await bidsCollection.insertOne(bid)
+            res.send(result)
+        })
 
 
         // Send a ping to confirm a successful connection
@@ -69,5 +141,3 @@ app.listen(port, () => {
 });
 
 
-// psazzadul1205
-// 1jOikDWMHRkiebal
